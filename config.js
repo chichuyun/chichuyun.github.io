eb5c65a886698352ac8b6fc2c22d2534727651b8
chichuyun <442307054@qq.com>
2022-08-22 12:05:39 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    lines: [
+        "亲爱的彤彤",
+        "我们在2022年4月30日相遇",
+        "在2022年7月3号皆为伴侣",
+        "You are my sun",
+        "Away love you",
+    ],
+    date: "2022-07-03T22:00:00",
+    names: ["Amaris", "Rupert"]
+}
